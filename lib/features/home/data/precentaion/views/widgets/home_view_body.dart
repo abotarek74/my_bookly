bfd4bce649af125019654868522ba7f2f5522318
abotarek74 <moustafatarek013@gmail.com>
2024-10-03 05:07:58 +0300

    BestSellerListViewItem part1

--- lib/features/home/data/precentaion/views/widgets/home_view_body.dart
+++ lib/features/home/data/precentaion/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/data/precentaion/views/widgets/custom_app_bar.dart';
 import 'package:bookly/features/home/data/precentaion/views/widgets/feature_list_view.dart';
@@ -21,7 +22,36 @@ class HomeViewBody extends StatelessWidget {
           Text(
             'Best Seller',
             style: Styles.titleMedium,
-          )
+          ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.red,
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.testImage),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
