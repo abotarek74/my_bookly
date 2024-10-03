1805937937e46378760c194a3b9bbff29a2eb54c
abotarek74 <moustafatarek013@gmail.com>
2024-10-03 23:15:47 +0300

    Best seller item done

--- lib/features/home/data/precentaion/views/widgets/home_view_body.dart
+++ lib/features/home/data/precentaion/views/widgets/home_view_body.dart
@@ -3,6 +3,7 @@ import 'package:bookly/features/home/data/precentaion/views/widgets/best_seller_
 import 'package:bookly/features/home/data/precentaion/views/widgets/custom_app_bar.dart';
 import 'package:bookly/features/home/data/precentaion/views/widgets/feature_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
