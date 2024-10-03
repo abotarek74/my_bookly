908042aee6dac4da1e1f150d4aafb5225801dea8
abotarek74 <moustafatarek013@gmail.com>
2024-10-03 04:21:42 +0300

    add featured list view to home

--- lib/features/home/data/precentaion/views/widgets/custom_app_bar.dart
+++ lib/features/home/data/precentaion/views/widgets/custom_app_bar.dart
@@ -8,7 +8,7 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 24),
+      padding: const EdgeInsets.only(right: 24, left: 24, top: 40, bottom: 20),
       child: Row(
         children: [
           Image.asset(
