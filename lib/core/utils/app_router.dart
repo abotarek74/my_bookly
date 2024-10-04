5671656467dd7f357819f83daa93ad7777bd3a7c
abotarek74 <moustafatarek013@gmail.com>
2024-10-04 20:34:56 +0300

    Create custom book details appbar

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,17 +1,26 @@
+import 'package:bookly/features/home/data/precentaion/views/book_details_view.dart';
 import 'package:bookly/features/home/data/precentaion/views/home_view.dart';
 import 'package:bookly/features/splash/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
-  static final router = GoRouter(routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const SplashView(),
-    ),
-    GoRoute(
-      path: kHomeView,
-      builder: (context, state) => const HomeView(),
-    ),
-  ]);
+  static const kBookDetailsView = '/bookDetailsView';
+
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const SplashView(),
+      ),
+      GoRoute(
+        path: kHomeView,
+        builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        path: kBookDetailsView,
+        builder: (context, state) => const BookDetailsView(),
+      ),
+    ],
+  );
 }
