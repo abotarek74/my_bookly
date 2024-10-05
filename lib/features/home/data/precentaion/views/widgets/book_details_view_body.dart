0e5bf7b24875857792102d10e3b9264e84bbfc21
abotarek74 <moustafatarek013@gmail.com>
2024-10-05 04:04:45 +0300

    add title & subtitle to book details

--- lib/features/home/data/precentaion/views/widgets/book_details_view_body.dart
+++ lib/features/home/data/precentaion/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/data/precentaion/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly/features/home/data/precentaion/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
@@ -9,13 +10,35 @@ class BookDetailsViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30),
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
-          CustomBookDetailsAppBar(),
+          const CustomBookDetailsAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.17),
-            child: CustomBookImage(),
+            padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+            child: const CustomBookImage(),
+          ),
+          const SizedBox(
+            height: 43,
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Text(
+            'The Jungle Book',
+            style: Styles.textStyle30.copyWith(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Opacity(
+            opacity: 0.7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
           ),
         ],
       ),
