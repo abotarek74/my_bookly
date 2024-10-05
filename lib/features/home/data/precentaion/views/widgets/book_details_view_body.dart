aacd1438063fcddca7f1eb4d40359465aa69a9aa
abotarek74 <moustafatarek013@gmail.com>
2024-10-05 04:17:38 +0300

    Add rating to book details

--- lib/features/home/data/precentaion/views/widgets/book_details_view_body.dart
+++ lib/features/home/data/precentaion/views/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/home/data/precentaion/views/widgets/book_rating.dart';
 import 'package:bookly/features/home/data/precentaion/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly/features/home/data/precentaion/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
@@ -39,6 +40,12 @@ class BookDetailsViewBody extends StatelessWidget {
                 fontWeight: FontWeight.w500,
               ),
             ),
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          const BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
           ),
         ],
       ),
