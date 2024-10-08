d266bf87c10cc8ea2d78e446412c1021cef30b05
abotarek74 <moustafatarek013@gmail.com>
2024-10-08 04:06:41 +0300

    create api service

---
+++ lib/features/home/data/repos/homr_repo_impl.dart
@@ -0,0 +1,18 @@
+import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomrRepoImpl implements HomeRepo {
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBoosks() {
+    // TODO: implement fetchBestSellerBoosks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeatuerdBoosks() {
+    // TODO: implement fetchFeatuerdBoosks
+    throw UnimplementedError();
+  }
+}
